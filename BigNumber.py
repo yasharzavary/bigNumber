9cad8cb7d125be2c0fb9d6dd7ccf9b1502e93a36
yashar <zavaryyashar009@gmail.com>
2023-11-28 20:24:25 +0330

    error class added, other functions not complete

--- BigNumber.py
+++ BigNumber.py
@@ -18,6 +18,18 @@
 
 """
 
+import string
+from re import search
+
+# exception class
+class BNError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.__message = message
+    
+    def __str__(self):
+        return f"Error happend : {self.__message}..."
+
 class BN:
     def __init__(self, num=0):
         """_summary_
@@ -26,20 +38,58 @@ class BN:
             num (, optional): value of the big number
         """
         # our list of digits
-        self.bigNumber = []
+        self.__bigNumber = []
         # fill the list depend on the type of the num
         if isinstance(num, str):
+            if search(r'[^1-9]', num):
+                raise BNError('you can\'t use any characters in your number, just number please')
             # adding to the list
             self.bigNumber = [number for number in num]
         elif isinstance(num, int):
             # change to the string and add it to the list
             self.bigNumber = [number for number in str(num)]
+        elif isinstance(num, list):
+            self.bigNumber = num
     
     def __len__(self):
+        """_summary_
+            return number of number's digits
+        Returns:
+            int: ...
+        """
         return len(self.bigNumber)
-            
+    
+    def insertNum(self, num, were):
+        """_summary_
+            this function will ad one number in specific part of the number list
+        Args:
+            num (optional): number that we want to add to the number list
+            were (int): place that we want to add it to the list
+        """
+        self.bigNumber.insert(num, were)
+         
     def __add__(self, other):
-        pass
+        lself = len(self)
+        lother = len(other)
+          
+        if lself > lother:
+            for _ in range(lself - lother):
+                self.insertNum(0, 0)
+        elif lother > lself:
+            for _ in range(lother - lself):
+                other.insertNum(0,0)
+                
+        result = []
+        carry = 0
+        for i in range(len(other)-1, -1, -1):
+            numTemp = (self.bigNumber[i] + other.bigNumber[i] + carry) % 10
+            carry = numTemp // 10
+            result.insert(numTemp, 0)
+        
+        
+            
+             
+        
     def __sub__(self, other):
         pass
     def __mul__(self, other):
