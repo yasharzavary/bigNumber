83eea098a2dfd46b6862d1c9d3092266e85937d1
yashar <zavaryyashar009@gmail.com>
2023-11-28 16:55:33 +0330

    big number class file added and magic functions ready to write

---
+++ BigNumber.py
@@ -0,0 +1,56 @@
+
+
+class BN:
+    def __init__(self, ):
+        pass
+    
+    def __add__(self, other):
+        pass
+    def __sub__(self, other):
+        pass
+    def __mul__(self, other):
+        pass
+    def __truediv__(self, other):
+        pass
+    def __floordiv__(self, other):
+        pass
+    def __mod__(self, other):
+        pass
+    def __pow__(self, other):
+        pass
+    def __rshift__(self, other):
+        pass
+    def __lshift__(self, other):
+        pass
+    def __lt__(self, other):
+        pass
+    def __gt__(self, other):
+        pass
+    def __le__(self, other):
+        pass
+    def __ge__(self, other):
+        pass
+    def __eq__(self, other):
+        pass
+    def __ne__(self, other):
+        pass
+    def __isub__(self, other):
+        pass
+    def __iadd__(self, other):
+        pass
+    def __imul__(self, other):
+        pass
+    def __ipow__(self, other):
+        pass
+    def __idiv__(self, other):
+        pass
+    def __ifloordiv__(self, other):
+        pass
+    
+    def __repr__(self):
+        pass
+    def __str__(self):
+        pass
+    def __del__(self):
+        pass
+    
